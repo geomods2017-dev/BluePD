88c1e0c851c1ff0f8d67f571939cb5363c8d656f
geomods2017-dev <geomods2017@gmail.com>
2026-03-26 23:23:06 -0400

    Update BluePDApp.swift

--- BluePD/BluePD/BluePDApp.swift
+++ BluePD/BluePD/BluePDApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct BluePDApp: App {
-    @StateObject private var sessionStore = SessionStore()
-
     var body: some Scene {
         WindowGroup {
-            RootView()
-                .environmentObject(sessionStore)
+            ContentView()
         }
     }
 }
