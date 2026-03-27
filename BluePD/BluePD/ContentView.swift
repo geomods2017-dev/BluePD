bce80b56a74dc3a42edd2e06f908a8cb507229c9
geomods2017-dev <geomods2017@gmail.com>
2026-03-27 01:27:56 -0400

    Update ContentView.swift

--- BluePD/BluePD/ContentView.swift
+++ BluePD/BluePD/ContentView.swift
@@ -1,7 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @AppStorage("hasAcceptedDisclaimer") private var hasAcceptedDisclaimer: Bool = false
+
     var body: some View {
         MainTabView()
+            .sheet(isPresented: .constant(!hasAcceptedDisclaimer)) {
+                FirstLaunchDisclaimerView()
+                    .interactiveDismissDisabled(true)
+            }
     }
 }
