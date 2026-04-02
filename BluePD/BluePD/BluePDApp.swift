b4c3221a2ea0ad5f89c67ebb358d84e0972a876c
geomods2017-dev <geomods2017@gmail.com>
2026-04-02 03:17:29 -0400

    Update BluePDApp.swift

--- BluePD/BluePD/BluePDApp.swift
+++ BluePD/BluePD/BluePDApp.swift
@@ -3,25 +3,15 @@ import SwiftUI
 @main
 struct BluePDApp: App {
     @AppStorage("isLoggedIn") private var isLoggedIn = false
-
-    @Environment(\.scenePhase) private var scenePhase
-    @State private var isUnlocked = false
+    @StateObject private var storeManager = StoreManager()
 
     var body: some Scene {
         WindowGroup {
-            Group {
-                if !isLoggedIn {
-                    LoginView()
-                } else if !isUnlocked {
-                    LockView(isUnlocked: $isUnlocked)
-                } else {
-                    ContentView()
-                }
-            }
-            .onChange(of: scenePhase) { phase in
-                if phase != .active {
-                    isUnlocked = false   // 🔒 Locks app when closed/backgrounded
-                }
+            if isLoggedIn {
+                ContentView()
+                    .environmentObject(storeManager)
+            } else {
+                LoginView()
             }
         }
     }
